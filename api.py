3e9198e0a70436f755b243c2f8e722a72b25b9cd
pbashyal-nmdp <pbashyal@nmdp.org>
2022-02-17 14:38:55 -0600

    - Updated requirements.txt files - Added `imgt-versions` endpoint

--- api.py
+++ api.py
@@ -1,7 +1,41 @@
 from flask import request
+import pandas
+import yaml
+from neo4j.exceptions import ServiceUnavailable
+from py2neo import Graph
 
 from my_project_template.model.allele import InvalidAllele
 from my_project_template.my_project_template import match
+
+with open("neo4j.yaml", "r") as neo4j_file:
+    neo_dict = yaml.safe_load(neo4j_file)
+
+
+def cypher_imgt_db_versions():
+    query = """
+    MATCH(g:GFE)-[e:HAS_WHO]-(w:WHO)
+	    WITH COLLECT(DISTINCT e.releases) AS releases
+	    UNWIND REDUCE(output=[], r IN releases| output + r) as dbs
+	    RETURN COLLECT(DISTINCT dbs) as HLA_DB_VERSIONS
+        ORDER BY HLA_DB_VERSIONS DESC
+    """
+    return query
+
+
+def list_all_db_releases(graph):
+    cypher = cypher_imgt_db_versions()
+    response = graph.run(cypher)
+    db_versions = response.next()
+    return db_versions["HLA_DB_VERSIONS"]
+
+
+def imgt_versions():
+    neo4j_url = neo_dict["neo4j_url"]
+    user = neo_dict["user"]
+    password = neo_dict["password"]
+    graph = Graph(neo4j_url, user=user, password=password)
+    dbs = list_all_db_releases(graph)
+    return {"imgt_versions": dbs}, 200
 
 
 def slug_match_controller():

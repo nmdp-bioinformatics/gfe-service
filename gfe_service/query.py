7ce6ac4136205c31f686cb25e21e423c8f879ec7
William Lemieux <WLemieux03@users.noreply.github.com>
2023-09-14 11:28:11 -0400

    Update query.py
    Edited the version to release to unify names

--- gfe_service/query.py
+++ gfe_service/query.py
@@ -21,8 +21,8 @@ def gfe_from_ipd():
 def all_gfe_from_locus():
     query = """
     MATCH (g:GFE)-[r:HAS_IPD_ALLELE]-(a:IPD_Allele)
-    WHERE g.locus = $locus AND $version IN r.releases
-    RETURN g.name AS gfe, a.name AS allele
+    WHERE g.locus = $locus AND $release IN r.releases
+    RETURN g.name AS gfe, a.name AS allele, r.releases AS release
     """
     return query
 
